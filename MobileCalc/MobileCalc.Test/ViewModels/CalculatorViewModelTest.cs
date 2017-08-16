cc08abae113826f4fc9b21e8c0612ec9f18ecba1
Lee Richardson <github@leerichardson.com>
2017-08-15 21:31:31 -0400

    Unit tests and a simple implementation of calculator view model

---
+++ MobileCalc/MobileCalc.Test/ViewModels/CalculatorViewModelTest.cs
@@ -0,0 +1,53 @@
+﻿using MobileCalc.ViewModels;
+using NUnit.Framework;
+
+namespace MobileCalc.Test.ViewModels
+{
+    [TestFixture]
+    public class CalculatorViewModelTest
+    {
+        [Test]
+        public void GivenStartup_WhenPressEquals_ThenDisplayIsZero()
+        {
+            var calculatorViewModel = new CalculatorViewModel();
+            calculatorViewModel.PressEquals();
+            Assert.AreEqual("0", calculatorViewModel.Display);
+        }
+
+        [Test]
+        public void GivenStartup_WhenPressOne_ThenDisplayIsOne()
+        {
+            var calculatorViewModel = new CalculatorViewModel();
+            calculatorViewModel.PressNumber(1);
+            Assert.AreEqual("1", calculatorViewModel.Display);
+        }
+
+        [Test]
+        public void GivenOneOnDisplay_WhenPressOneAgain_ThenDisplayIsEleven()
+        {
+            var calculatorViewModel = new CalculatorViewModel();
+            calculatorViewModel.PressNumber(1);
+            calculatorViewModel.PressNumber(1);
+            Assert.AreEqual("11", calculatorViewModel.Display);
+        }
+
+        [Test]
+        public void GivenOneOnDisplay_WhenPressClear_ThenDisplayIsZero()
+        {
+            var calculatorViewModel = new CalculatorViewModel();
+            calculatorViewModel.PressNumber(1);
+            calculatorViewModel.PressClear();
+            Assert.AreEqual("0", calculatorViewModel.Display);
+        }
+
+        [Test]
+        public void GivenOneOnDisplay_WhenPressClearThenPressOne_ThenDisplayIsOne()
+        {
+            var calculatorViewModel = new CalculatorViewModel();
+            calculatorViewModel.PressNumber(1);
+            calculatorViewModel.PressClear();
+            calculatorViewModel.PressNumber(1);
+            Assert.AreEqual("0", calculatorViewModel.Display);
+        }
+    }
+}
---
+++ MobileCalc/MobileCalc/MobileCalc/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace MobileCalc.ViewModels
+{
+    public class CalculatorViewModel
+    {
+        public string Display { get; private set; }
+        private int _currentNumber = 0;
+
+        public void PressEquals()
+        {
+            _currentNumber = 0;
+            UpdateDisplay();
+        }
+
+        public void PressClear()
+        {
+            _currentNumber = 0;
+            UpdateDisplay();
+        }
+
+        public void PressNumber(int number)
+        {
+            _currentNumber = _currentNumber * 10;
+            _currentNumber = _currentNumber + number;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            Display = _currentNumber.ToString();
+        }
+    }
+}
