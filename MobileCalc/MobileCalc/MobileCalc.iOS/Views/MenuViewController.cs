87adb91d04d2f309aee3ddcd834a75c98756a6b5
Lee Richardson <github@leerichardson.com>
2017-11-28 21:52:32 -0500

    Display a list of pages in the left in a table view

--- MobileCalc/MobileCalc/MobileCalc.iOS/Views/MenuViewController.cs
+++ MobileCalc/MobileCalc/MobileCalc.iOS/Views/MenuViewController.cs
@@ -1,13 +1,45 @@
-﻿using UIKit;
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+using MobileCalc.ViewModels;
+using UIKit;
 
 namespace MobileCalc.iOS.Views
 {
-    public class MenuViewController : UIViewController
+    public class MenuViewController : UITableViewController
     {
+        private const string ReuseIdentifier = "PageId";
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            View.BackgroundColor = UIColor.White;
+
+            var mainDrawerViewModel = new MainDrawerViewModel();
+            TableView.Source = new PagesTableSource(mainDrawerViewModel.PageNames);
+            TableView.RegisterClassForCellReuse(typeof(UITableViewCell), ReuseIdentifier);
+        }
+
+        public class PagesTableSource : UITableViewSource
+        {
+            private readonly List<string> _pages;
+
+            public PagesTableSource(List<string> pages)
+            {
+                _pages = pages;
+            }
+
+            public override nint RowsInSection(UITableView tableview, nint section)
+            {
+                return _pages.Count;
+            }
+
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                var page = _pages[indexPath.Row];
+                var cell = tableView.DequeueReusableCell(ReuseIdentifier);
+                cell.TextLabel.Text = page;
+                return cell;
+            }
         }
     }
 }
